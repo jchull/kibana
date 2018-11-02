c97de46512c4eaf89758cfacd612e65c5f70398a
jchull <jchull@gmail.com>
2018-11-01 23:15:21 -0600

    Remove angular legend from use, added color picker, details panel, moved legend creation to handler, reversed column direction since layout cleans the container on render

--- src/ui/public/vislib/lib/chart_legend.js
+++ src/ui/public/vislib/lib/chart_legend.js
@@ -33,6 +33,16 @@ export function VislibLibChartLegendProvider() {
     '#E0F9D7', '#FCEACA', '#CFFAFF', '#F9E2D2', '#FCE2DE', '#BADFF4', '#F9D9F9', '#DEDAF7'  //7
   ];
 
+  const COLOR_CHOICES = [
+    '#3F6833', '#967302', '#2F575E', '#99440A', '#58140C', '#052B51', '#511749', '#3F2B5B', //6
+    '#508642', '#CCA300', '#447EBC', '#C15C17', '#890F02', '#0A437C', '#6D1F62', '#584477', //2
+    '#629E51', '#E5AC0E', '#64B0C8', '#E0752D', '#BF1B00', '#0A50A1', '#962D82', '#614D93', //4
+    '#7EB26D', '#EAB839', '#6ED0E0', '#EF843C', '#E24D42', '#1F78C1', '#BA43A9', '#705DA0', // Normal
+    '#9AC48A', '#F2C96D', '#65C5DB', '#F9934E', '#EA6460', '#5195CE', '#D683CE', '#806EB7', //5
+    '#B7DBAB', '#F4D598', '#70DBED', '#F9BA8F', '#F29191', '#82B5D8', '#E5A8E2', '#AEA2E0', //3
+    '#E0F9D7', '#FCEACA', '#CFFAFF', '#F9E2D2', '#FCE2DE', '#BADFF4', '#F9D9F9', '#DEDAF7'  //7
+  ];
+
   const LEGEND_COLLAPSE_ICON = 'fa-chevron-circle';
   const legendPositionMap = {
     top: {
@@ -343,6 +353,31 @@ export function VislibLibChartLegendProvider() {
       }
     }
 
+    // addEvents(element) {
+    //   const events = this.events;
+    //
+    //   return element
+    //     .call(events.addHoverEvent())
+    //     .call(events.addMouseoutEvent())
+    //     .call(events.addClickEvent());
+    // }
+
+    setColor(label, color) {
+      const uiState = this.visConfig.data.uiState;
+      const colors = uiState.get('vis.colors') || {};
+      if(colors[label] === color) {
+        delete colors[label];
+      } else {
+        colors[label] = color;
+      }
+      uiState.set('vis.colors', colors);
+      uiState.emit('colorChanged');
+    }
+
+
+    toggle() {
+      return this.data.uiState.set('vis.legendOpen', !this.expanded);
+    }
 
     /**
      * Handles mouseenter event on legend labels
